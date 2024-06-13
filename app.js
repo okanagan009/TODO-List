b1f2906b09090f648748af8a767fe7018a65e5d7
okanagan009 <serezharostov161@gmail.com>
2024-06-13 09:06:31 +0800

    added tasks to the page

--- app.js
+++ app.js
@@ -1,16 +1,43 @@
 // Globals
+const todoList = document.getElementById('todo-list');
 let todos = [];
 let users = [];
 
 // Attach events 
 document.addEventListener('DOMContentLoaded', initApp);
 
+// Basic logic
+function getUserName(userId) {
+    const user = users.find(u => u.id === userId);
+    return user.name;
+}
+
+function printTodo({id, userId, title, completed}) {
+    const li = document.createElement('li');
+    li.className = 'todo-item';
+    li.dataset.id = id;
+    li.innerHTML = `<span>${title} <i>by</i> <b>${getUserName(userId)}</b></span>`
+
+    const status = document.createElement('input');
+    status.type = 'checkbox';
+    status.checked = completed;
+
+    const close = document.createElement('span')
+    close.innerHTML = '&times;';
+    close.className = 'close';
+
+    li.prepend(status);
+    li.append(close);
+
+    todoList.prepend(li)
+}
+
 // Event logic
 function  initApp() {
     Promise.all([getAllTodos(), getAllUsers()]).then(values => {
         [todos, users] = values;
 
-        
+        todos.forEach((todo) => printTodo(todo));
     })
 }
 
